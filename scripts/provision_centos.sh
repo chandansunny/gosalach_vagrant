33349c9e92ce4e1445f6f16a1b55866e907cb0b8
Chandan Gosala <chandangosala@chandans-mbp.attlocal.net>
2017-02-10 15:21:37 -0800

    Adding Vagrantfile for centos

---
+++ scripts/provision_centos.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+#Update centos with any patches
+yum update -y --exclude=kernel
+
+#Tools
+yum install -y git unzip screen
+
+#Appache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+suod service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrnat /var/www/html
+
+service httpd start
+
+#PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+#MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+sudo service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+#DOwnload Content
+cd /vagrant
+sudo -u vagrant wget -q <URL>
+sudo -u vagrnat wget -q <URL>
+
+sudo service httpd restart
