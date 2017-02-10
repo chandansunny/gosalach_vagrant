9dea613fc6bd1efd1d609b701b596eaab5eafe9d
Chandan Gosala <chandangosala@chandans-mbp.attlocal.net>
2017-02-10 15:34:53 -0800

    typo

--- scripts/provision_centos.sh
+++ scripts/provision_centos.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/bash
 
-#Update centos with any patches
+# Update centos with any patches
 yum update -y --exclude=kernel
 
-#Tools
+# Tools
 yum install -y git unzip screen
 
-#Appache
+# Apache
 yum install -y httpd httpd-devel httpd-tools
 chkconfig --add httpd
 chkconfig httpd on
-suod service httpd stop
+service httpd stop
 
 rm -rf /var/www/html
-ln -s /vagrnat /var/www/html
+ln -s /vagrant /var/www/html
 
 service httpd start
 
-#PHP
+# PHP
 yum install -y php php-cli php-common php-devel php-mysql
 
-#MySQL
+# MySQL
 yum install -y mysql mysql-server mysql-devel
 chkconfig --add mysqld
 chkconfig mysqld on
 
-sudo service mysqld start
+service mysqld start
 
 mysql -u root -e "SHOW DATABASES";
 
-#DOwnload Content
+# Download Content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/chandansunny/gosalach_vagrant/master/files/index.html
 sudo -u vagrnat wget -q https://raw.githubusercontent.com/chandansunny/gosalach_vagrant/master/files/info.php
 
-sudo service httpd restart
+service httpd restart
